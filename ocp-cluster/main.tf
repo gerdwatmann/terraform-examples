aec181eda295d7d0e9ea7ceedf211d83165a8d8d
Gerd Watmann <watmann@de.ibm.com>
2020-06-18 21:26:45 +0200

    added logdna + sysdig

--- ocp-cluster/main.tf
+++ ocp-cluster/main.tf
@@ -3,9 +3,28 @@ data "ibm_resource_group" "resource_group" {
   name = "${var.resource_group}"
 }
 
+resource "ibm_resource_instance" "logdna" {
+  name              = "${var.unique_id}-logdna"
+  service           = "logdna"
+  plan              = "${var.logging_plan}"
+  location          = "${var.ibm_region}"
+  resource_group_id = "${data.ibm_resource_group.resource_group.id}"
+  parameters = {
+    service-endpoints = "${var.end_points}"
+  }
+}
+
+resource "ibm_resource_instance" "sysdig" {
+  name              = "${var.unique_id}-sysdig"
+  service           = "sysdig-monitor"
+  plan              = var.sysdig_plan
+  location          = "${var.ibm_region}"
+  resource_group_id = "${data.ibm_resource_group.resource_group.id}"
+}
+
 resource "ibm_container_cluster" "cluster" {
   resource_group_id = "${data.ibm_resource_group.resource_group.id}"
-  name              = "${var.cluster_name}"
+  name              = "${var.unique_id}-cluster"
   datacenter        = "${var.datacenter}"
   default_pool_size = "${var.default_pool_size}"
   machine_type      = "${var.machine_type}"
--- ocp-cluster/variables.tf
+++ ocp-cluster/variables.tf
@@ -6,6 +6,38 @@ variable "ibmcloud_api_key" {
   description = "IBM Cloud API Key"
 }
 
+variable unique_id {
+  description = "Prefix for all resources created in the module. Must begin with a letter."
+  default     = "bootcamp"
+}
+
+##############################################################################
+# LogDNA Logging Variables
+##############################################################################
+
+variable "logging_plan" {
+  default = "30 day"
+}
+
+variable end_points {
+  description = "Sets the endpoints for the resources provisioned. Can be `public` or `private`"
+  default     = "public"
+}
+
+##############################################################################
+# Sysdig Logging Variables
+##############################################################################
+
+variable "sysdig_plan" {
+  description = "Plan for your Sysdig instance in IBM Cloud."
+  default = "graduated-tier"
+}
+
+
+##############################################################################
+# OpenShift Cluster Variables
+##############################################################################
+
 variable "machine_type" {
   default = "b2c.8x32"
 }
@@ -27,9 +59,11 @@ variable "private_vlan_id" {}
 variable "public_vlan_id" {}
 
 variable "cluster_name" {
-  default = "bootcamp-cluster"
+  default = "cluster"
 }
 variable kube_version {
   default = "4.3_openshift"
 }
+##############################################################################
+
 
