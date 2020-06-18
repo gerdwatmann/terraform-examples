f5d7e56e6bd736de6822d4b143ca3ce9ef862581
Gerd Watmann <watmann@de.ibm.com>
2020-06-18 20:27:11 +0200

    quotes missing

--- ocp-cluster/main.tf
+++ ocp-cluster/main.tf
@@ -12,5 +12,5 @@ resource "ibm_container_cluster" "cluster" {
   hardware          = "${var.hardware}"
   kube_version      = "${var.kube_version}"
   public_vlan_id    = "${var.public_vlan_id}"
-  private_vlan_id   = "${var.private_vlan_id}  
+  private_vlan_id   = "${var.private_vlan_id}"  
 }
